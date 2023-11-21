f220939a1579adf2b5e1a8b0585ae939c9e6aad3
Florian Gschoesser <f.gschoesser@student.uibk.ac.at>
2023-11-21 11:26:10 +0100

    reduced diag, changed inputs

--- src/mima/diag.h
+++ src/mima/diag.h
@@ -43,12 +43,12 @@ std::vector<Record> diagnostics2d_static_list = {
 
 // time - dependent output (called periodically)
 std::map<std::string, std::vector<Record>> diagnostics2d_list = {
-    { "global", {
+    { "local", {/*
     {"ne", "Electron density in 2d",
         []( dg::x::DVec& result, Variables& v) {
             dg::blas1::copy( v.rhs.var(), result);
         }
-    },/*
+    },
     {"gy", "Ion-gyro-center density in 2d",
         []( dg::x::DVec& result, Variables& v) {
             dg::blas1::copy( v.rhs.var(1), result);
@@ -73,68 +73,13 @@ std::map<std::string, std::vector<Record>> diagnostics2d_list = {
         []( dg::x::DVec& result, Variables& v) {
             dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.var(1), 0., result);
         }
-    },
-    {"lapPhi", "+Delta Phi",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.phi(0), 0., result);
-        }
-    },
-    {"Se", " ne ln ne",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::transform( v.rhs.var(0), result, dg::LN<double>());
-            dg::blas1::pointwiseDot(  1., v.rhs.var(0), result, 0., result);
-        }
-    },
-    {"Si", " tau_i ni ln ni",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::transform( v.rhs.var(1), result, dg::LN<double>());
-            dg::blas1::pointwiseDot(  v.p.tau, v.rhs.var(1), result, 0., result);
-        }
-    },
-    {"U", " 0.5 ni u_E^2",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::pointwiseDot( 0.5, v.rhs.var(1),
-                     v.rhs.uE2(), 0., result);
-        }
-    }*/
-                }
-    },
-    { "local", {
-    {"ne", "Electron density in 2d",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy( v.rhs.var(), result);
-        }
-    },/*
-    {"gy", "Ion-gyro-center density in 2d",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy( v.rhs.var(1), result);
-        }
     },*/
-    {"phi", "Electric potential",
+    {"vort", "Vorticity",
         []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy(v.rhs.phi(), result);
+            //dg::blas2::gemv( 1., v.rhs.laplacianM(), v.rhs.phi(), 0., result);
+            dg::blas1::copy(v.rhs.chi(), result);
         }
     },/*
-    {"psi", "Gyro-center potential",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy(v.rhs.phi(1), result);
-        }
-    },
-    {"lapNe", "+Delta ne",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.var(0), 0., result);
-        }
-    },
-    {"lapNi", "+Delta ni",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.var(1), 0., result);
-        }
-    },
-    {"lapPhi", "+Delta Phi",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.phi(0), 0., result);
-        }
-    },
     {"Se", " 0.5 ne^2 ",
         []( dg::x::DVec& result, Variables& v) {
             dg::blas1::pointwiseDot( 1., v.rhs.var(0), v.rhs.var(0), 0., result);
@@ -145,12 +90,12 @@ std::map<std::string, std::vector<Record>> diagnostics2d_list = {
             dg::blas1::pointwiseDot(  v.p.tau, v.rhs.var(1), v.rhs.var(1), 0.,
                     result);
         }
-    },*/
+    },
     {"U", " 0.5 u_E^2",
         []( dg::x::DVec& result, Variables& v) {
             dg::blas1::axpby( 0.5, v.rhs.uE2(), 0., result);
         }
-    }
+    }*/
                 }
     },
     { "drift-global", {
@@ -202,100 +147,9 @@ std::map<std::string, std::vector<Record>> diagnostics2d_list = {
         }
     }
                 }
-    },
-    { "gravity-global", {
-    {"n", "Electron density in 2d",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy( v.rhs.var(), result);
-        }
-    },/*
-    {"rho", "Vorticity",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy( v.rhs.var(1), result);
-        }
-    },
-    {"phi", "Electric potential",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy(v.rhs.phi(0), result);
-        }
-    },
-    {"psi", "Gyro-center potential",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy(v.rhs.phi(1), result);
-        }
-    },
-    {"lapN", "+Delta n",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.var(0), 0., result);
-        }
-    },
-    {"lapRho", "+Delta rho",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.var(1), 0., result);
-        }
-    },
-    {"lapPhi", "+Delta Phi",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.phi(0), 0., result);
-        }
-    },
-    {"S", " 0.5 n^2",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::pointwiseDot(  0.5, v.rhs.var(0), v.rhs.var(0), 0., result);
-        }
-    },*/
-    {"U", " 0.5 u_E^2",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::axpby( 0.5, v.rhs.uE2(), 0., result);
-        }
-    }
-                }
-    },
-    { "gravity-local", {
-    {"n", "Electron density in 2d",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy( v.rhs.var(), result);
-        }
-    },/*
-    {"rho", "Vorticity",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy( v.rhs.var(1), result);
-        }
-    },
-    {"phi", "Electric potential",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::copy(v.rhs.phi(0), result);
-        }
-    },
-    {"lapN", "+Delta n",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.var(0), 0., result);
-        }
-    },
-    {"lapRho", "+Delta rho",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.var(1), 0., result);
-        }
-    },
-    {"lapPhi", "+Delta Phi",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas2::gemv( -1., v.rhs.laplacianM(), v.rhs.phi(0), 0., result);
-        }
-    },
-    {"S", " 0.5 n^2",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::pointwiseDot(  0.5, v.rhs.var(0), v.rhs.var(0), 0., result);
-        }
-    },*/
-    {"U", " 0.5 u_E^2",
-        []( dg::x::DVec& result, Variables& v) {
-            dg::blas1::axpby( 0.5, v.rhs.uE2(), 0., result);
-        }
-    }
-                }
     }
 };
 
 
-} //namespace impurities
+} //namespace mima
 
